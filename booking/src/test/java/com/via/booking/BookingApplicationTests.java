c5776068d332e8bf86fdb178232041131bfdcb41
Valentin Alberto <valalberto0214@gmail.com>
2025-02-18 21:54:04 +0800

    initial code commit

---
+++ booking/src/test/java/com/via/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.via.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
